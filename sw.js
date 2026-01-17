b94bd077d9f48259b3e4d794cd940d1fbe3e2a88
Manish Gupta <manishgupta942667@gmail.com>
2026-01-17 21:10:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,59 +1,25 @@
-const cacheName = 'pharma-quiz-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './icon.png',
-  
-  // ORIGINAL PHARMACEUTICS TOPICS
-  './history of pharmacy.html',
-  './packaging material.html',
-  './tablets.html',
-  './capsule.html',
-  './size reduction.html',
-  
-  // ORIGINAL PHARMACOLOGY TOPICS
-  './route of administration.html',
-  './Pharmacokinetics & dynamic , AD, clinical trial.html',
-  './Cholinergic & Anticholinergic drugs.html',
-  './Adrenergic & Antiadrenergic drugs.html',
-  
-  // NEW SUBJECT TOPIC BLOCKS (A, B, C)
-  './cogno-a.html', './cogno-b.html', './cogno-c.html',
-  './hap-a.html', './hap-b.html', './hap-c.html',
-  './biochem-a.html', './biochem-b.html', './biochem-c.html',
-  './chem-a.html', './chem-b.html', './chem-c.html',
-  './community-a.html', './community-b.html', './community-c.html'
+const CACHE_NAME = 'rx-aspirant-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/manifest.json'
 ];
 
-// 1. Install Event: Saves files to the phone
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching assets...');
-      return cache.addAll(assets);
+// Install Service Worker and Cache Files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-// 2. Fetch Event: Serves files from cache when offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// 3. Activate Event: Cleans up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
-      );
+// Serve Cached Content when Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
